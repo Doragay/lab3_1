52da97f4c318eedf55bf2548aeb5e0da15cd10c3
Daria <nik-fa01@yandex.ru>
2024-06-06 16:16:42 +0300

    Добавила выбор стратегии в консоли и бесконечный цикл

--- main.cpp
+++ main.cpp
@@ -1,43 +1,54 @@
-#include <iostream>
+#include <QCoreApplication>
 #include <QTextStream>
+#include <iostream>
 #include "foldersizegrouper.h"
 #include "typesizegrouper.h"
+#include <stdexcept>
 
-enum class GroupingMode
-{
-    Folders,
-    Types
-};
-
-QMap<QString, qint64> groupFileSizes(const QString& path, GroupingMode mode, qint64& total)
+//группирует файлы в зависимости от указанного режима группировки
+QMap<QString, qint64> groupFileSizes(const QString& path, const QString& mode, qint64& total)
 {
     QMap<QString, qint64> sizes;
-    if (mode == GroupingMode::Folders) {
+    if (mode == "folders") {
         sizes = FolderSizeGrouper().group(path, total);
-    } else {
+    } else if (mode == "types") {
         sizes = TypeSizeGrouper().group(path, total);
     }
     return sizes;
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    QTextStream in(stdin);
-    QTextStream out(stdout);
+    QCoreApplication app(argc, argv);
 
-    QString path = in.readLine();
+    while (true) {
+        QTextStream cin(stdin);
+        QTextStream cout(stdout);
 
-    qint64 total = 0;
-    QMap<QString, qint64> group = groupFileSizes(path, GroupingMode::Folders, total);
+        QString path = cin.readLine();
+        QString mode = cin.readLine();
 
-    for (auto it = group.keyValueBegin(); it != group.keyValueEnd(); ++it) {
-        QString spercent = "*";
-        if (total != 0) {
-            auto percent = 100 * static_cast<double>(it->second) / total;
-            spercent = percent > 0.01 ? QString::number(percent, 'f', 2) : "< 0.01";
+        qint64 total = 0;
+        QMap<QString, qint64> group;
+
+        try {
+            group = groupFileSizes(path, mode, total);
+        } catch (std::exception& ex) {
+            cout << "Error: " << ex.what() << '\n';
+            continue;
         }
-        out << it->first << " --- " << spercent << "%\n";
-    }
-    return 0;
 
+        for (auto it = group.keyValueBegin(); it != group.keyValueEnd(); ++it) {
+            QString ssize = QString::number(it->second / 1024);
+            QString spercent = "*";
+            if (total != 0) {
+                auto percent = 100 * static_cast<double>(it->second) / total;
+                spercent = percent > 0.01 ? QString::number(percent, 'f', 2) : "< 0.01";
+            }
+            cout << it->first << " --- " << ssize << "kb --- " << spercent << "%\n";
+        }
+        cout << "---------------------------------\n";
+    }
+
+    return app.exec();
 }
