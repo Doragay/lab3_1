eba0ca57667d5e26058394e96b06adb771f29ef1
Daria <nik-fa01@yandex.ru>
2024-06-08 12:50:07 +0300

    Убрала блок кода вычислений из контекста

--- main.cpp
+++ main.cpp
@@ -3,37 +3,17 @@
 #include "foldersizegrouper.h"
 #include "typesizegrouper.h"
 #include <stdexcept>
-
 class Context
 {
 public:
     void setSizeGrouper(SizeGrouper* grouper)
     {
-        _grouper.reset(grouper);//устанавливает новый объект
+        _grouper.reset(grouper);
     }
 
-    QList<QString> makeGroup(const QString& path)//создаем список строк в заданном пути
+    QMap<QString, qint64> makeGroup(const QString& path, qint64& total)
     {
-        if (!_grouper) {
-            return {};
-        }
-
-        QList<QString> strreprs;//объявление списка строк
-    //Заполнение списка строк
-        qint64 total = 0;
-        auto group = _grouper->group(path, total);//Вызываем метод group, который возвращает карту
-        for (auto it = group.keyValueBegin(); it != group.keyValueEnd(); ++it) { //проходимся по всем элементам карты, пока не достигнем последнего элемента
-            QString ssize = QString::number(it->second / 1024);//преобразуем размер группы в килобайты, а затем в строку
-            QString spercent = 0;
-            if (total != 0) {
-                auto percent = 100 * static_cast<double>(it->second) / total;//вычисляем процентное соотношение размера текущей группы файлов относительно общего размера всех файлов
-                spercent = percent > 0.01 ? QString::number(percent, 'f', 2) : "< 0.01";//Форматируем процент в строку с двумя знаками после запятой, если он больше 0.01
-            }
-            QString str = it->first + " --- " + ssize + "kb --- " + spercent + '%';//итоговая строка
-            strreprs.append(str);//добавляет итоговую строку в список
-        }
-
-        return strreprs;
+        return _grouper ? _grouper->group(path, total) : QMap<QString, qint64>{};
     }
 
 private:
@@ -41,7 +21,8 @@ private:
 };
 
 
-SizeGrouper* makeGrouper(const QString& mode)
+
+SizeGrouper* makeGrouper(const QString& mode)//принимает режим группировки и возваращает указатель
 {
     if (mode == "folders") {
         return new FolderSizeGrouper;//создат новый объект
@@ -66,18 +47,25 @@ int main(int argc, char** argv)
         QString path = cin.readLine();//считывает строку из ввода, представляющая путь
         QString mode = cin.readLine();
 
-        QList<QString> strlist;
+        qint64 total = 0;
+        QMap<QString, qint64> group;
         try {
             ctx.setSizeGrouper(makeGrouper(mode));// создает экземпляр класса, который выбирается в соответствии с переданным режимом
-            strlist = ctx.makeGroup(path); //метод создает группу файлов в указанном пути
+            group = ctx.makeGroup(path, total); //метод создает группу файлов в указанном пути
         } catch (std::exception& ex) {//выбрасываются исключения
             cout << "Error: " << ex.what() << '\n';
             continue;
         }
-
-        for (const auto& x : strlist) {//проходит по каждому элементу в списке строк
-            cout << x << '\n';
+        for (auto it = group.keyValueBegin(); it != group.keyValueEnd(); ++it) { //проходимся по всем элементам карты, пока не достигнем последнего элемента
+            QString ssize = QString::number(it->second / 1024);//преобразуем размер группы в килобайты, а затем в строку
+            QString spercent = "*";
+            if (total != 0) {
+                auto percent = 100 * static_cast<double>(it->second) / total;//вычисляем процентное соотношение размера текущей группы файлов относительно общего размера всех файлов
+                spercent = percent > 0.01 ? QString::number(percent, 'f', 2) : "< 0.01";//Форматируем процент в строку с двумя знаками после запятой, если он больше 0.01
+            }
+           cout << it->first << " --- " << ssize << "kb --- " << spercent << "%\n";
         }
+
         cout << "---------------------------------\n";
     }
 
